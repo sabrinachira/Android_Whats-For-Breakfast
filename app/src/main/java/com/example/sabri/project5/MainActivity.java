5bdc2f6436c8819b13ec7b02705d9b46a79e737b
sabrinachira <sabrina.chira.14@cnu.edu>
2018-04-16 15:50:42 -0400

    Buttons created & their methods' setup created.

--- app/src/main/java/com/example/sabri/project5/MainActivity.java
+++ app/src/main/java/com/example/sabri/project5/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sabri.project5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void add_one_egg(View view) {
+    }
+
+    public void add_two_egg(View view) {
+    }
+
+    public void subtract_one_egg(View view) {
+    }
+
+    public void make_breakfast(View view) {
     }
 }
