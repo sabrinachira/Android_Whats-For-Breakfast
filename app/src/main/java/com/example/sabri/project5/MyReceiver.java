dc088f8c3f06967c036bb4b9d6491814aac1d41d
sabrinachira <sabrina.chira.14@cnu.edu>
2018-04-16 16:55:20 -0400

    Logic added in MyReceiver.

--- app/src/main/java/com/example/sabri/project5/MyReceiver.java
+++ app/src/main/java/com/example/sabri/project5/MyReceiver.java
@@ -10,11 +10,34 @@ import android.content.Intent;
 
 public class MyReceiver extends BroadcastReceiver {
     int eggCountReceiver;
+    int current_egg_count;
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent myIntent = new Intent(context,MyService.class);
         intent.getIntExtra("@string/code", eggCountReceiver);
 
+        if(eggCountReceiver == 1){
+            current_egg_count +=1;
+            //notification with @string/one_egg_added
+        }else if(eggCountReceiver == 2){
+            current_egg_count +=2;
+            //notification with @string/two_eggs_added
+        }else if(eggCountReceiver == -1) {
+            if(current_egg_count == 0){
+                //notification with @string/no_eggs
+            }else{
+                current_egg_count -=1;
+                //notification with @string/subtract
+            }
+        }else if(eggCountReceiver == 6){
+            if(current_egg_count >= 6){
+                current_egg_count -=6;
+                //notification with @string/make_breakfast
+            }else{
+                //notification with @string/gruel
+            }
+        }
+        
         context.startService(myIntent);
     }
 }
