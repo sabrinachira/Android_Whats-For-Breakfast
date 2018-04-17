5740797614926fde19c3fc8cc996cfb8342a74f1
DillonSykes <dsykes321@gmail.com>
2018-04-16 20:04:39 -0400

    Receiver and Service class.

---
+++ app/src/main/java/com/example/sabri/project5/MyReceiver.java
@@ -0,0 +1,17 @@
+package com.example.sabri.project5;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by jaydo on 4/16/2018.
+ */
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent myIntent = new Intent(context,MyService.class);
+        context.startService(myIntent);
+    }
+}
---
+++ app/src/main/java/com/example/sabri/project5/MyService.java
@@ -0,0 +1,16 @@
+package com.example.sabri.project5;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by jaydo on 4/16/2018.
+ */
+
+public class MyService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
